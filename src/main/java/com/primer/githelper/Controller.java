fd9aa86d58b1e7e6ada08684230e78168ce3368f
primerxiao <primerxiao@163.com>
2020-03-27 12:07:45 +0800

    添加gitlabapi提交提交合并请求，释放双手

--- src/main/java/com/primer/githelper/Controller.java
+++ src/main/java/com/primer/githelper/Controller.java
@@ -139,8 +139,7 @@ public class Controller implements Initializable {
             String userName = map.get("USERNAME");// 获取用户名
             String fileName ="C:\\Users\\"+userName+"\\Documents\\"+LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HHmmss"))+".xlsx";
 
-            String path = this.getClass().getClassLoader().getResource("//").getPath().replaceFirst("/","");
-            String tpFileName = path + "template.xlsx";
+            String tpFileName = "./template.xlsx";
 
             // 这里 需要指定写用哪个class去读，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
             // 如果这里想使用03 则 传入excelType参数即可
